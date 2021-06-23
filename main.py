e6334cbef932fd7ec81011538f2d72f158ab05a4
sternamos <sternamos@gmail.com>
2021-06-23 10:10:07 +0300

    Test amos

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
+# Press ⌃R to execute it or replace it with your code. d
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
