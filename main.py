664d773a294c3b48ba21555b48e441167c3132b0
sternamos <sternamos@gmail.com>
2021-06-23 10:33:24 +0300

    S

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code. dv
+# Press ⌃R to execute it or replace it with your code. dvD
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 import tensorflow as tf
