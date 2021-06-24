f7eb3fbcf66b333b02ba59aa9a73b89787551441
bar246802 <bar246802@gmai;.com>
2021-06-24 19:04:43 +0300

    test pymol

---
+++ create_env.sh
@@ -0,0 +1,35 @@
+##!bin/bash
+##Creating the vertial environment
+#mkdir project4
+#cd project4
+#virtualenv -p /usr/bin/python3 newenv
+#source newenv/bin/activate.csh
+#
+#pip install tensorflow~=2.0.0b1
+#pip install numpy~=1.16.6
+#pip install bio~=0.5.0
+#pip install matplotlib~=3.4.2
+#pip install pymolPy3~=0.1.2
+#pip install logomaker~=0.8
+#pip install pandas~=1.2.5
+#pip tqdm~=4.61.1
+#module load bioinfo
+#moudle load pymol
+
+#!bin/bash
+#Creating the vertial environment
+mkdir project4
+cd project4
+virtualenv -p /usr/bin/python3.7 newenv
+source newenv/bin/activate.csh
+
+#pip install tensorflow~=2.0.0b1
+#pip install numpy~=1.16.6
+#pip install bio~=0.5.0
+#pip install matplotlib~=3.4.2
+#pip install pymolPy3~=0.1.2
+#pip install logomaker~=0.8
+#pip install pandas~=1.2.5
+#pip tqdm~=4.61.1
+#module load bioinfo
+#moudle load pymol
