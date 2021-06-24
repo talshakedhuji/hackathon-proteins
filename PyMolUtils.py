b6d1eab02c10be9acc92cd1e8ae79157362acd2e
bar246802 <bar246802@gmail.com>
2021-06-24 15:48:50 +0300

    Pymol Utils

--- PyMolUtils.py
+++ PyMolUtils.py
@@ -3,8 +3,13 @@ import pymolPy3
 import platform
 import subprocess
 
+import os
+import pymolPy3
+import platform
+import subprocess
+
 def create_pdb_img(ref_pdb_path, target_pdb_path, out_path_dir):
-    if platform.system() == 'Linux':
+    if platform.system() != 'Linux':
         try:
             # Run command install
             try:
@@ -35,20 +40,21 @@ def create_pdb_img(ref_pdb_path, target_pdb_path, out_path_dir):
                 print(f"Error - Target PDB file isn't found, please verify path: {target_pdb_path}")
                 return
 
-            # pm = pymolPy3.pymolPy3()
+            pm = pymolPy3.pymolPy3()
             target_file_name = os.path.basename(target_pdb_path)
             target_file_no_type = target_file_name.split(".")[0]
             ref_file_name = os.path.basename(ref_pdb_path)
             ref_file_no_type = ref_file_name.split(".")[0]
             # Run commands
-            print(f"load {ref_pdb_path}")
-            print(f"color yellow, {ref_file_no_type}")
-            print(f"load {target_pdb_path}")
-            print(f"color green, {target_file_no_type}")
-            print(f"align {ref_file_no_type}, {target_file_no_type}")
-            print(f"center")
-            print(f"zoom")
-            print(f"png {out_path_dir}/align_img")
+            pm(f"load {ref_pdb_path}")
+            pm(f"color yellow, {ref_file_no_type}")
+            pm(f"load {target_pdb_path}")
+            pm(f"color green, {target_file_no_type}")
+            pm(f"align {ref_file_no_type}, {target_file_no_type}")
+            pm(f"center")
+            pm(f"zoom")
+            pm(f"png {out_path_dir}/align_img")
+            print(f"Done! the image is here: '{out_path_dir}/align_img.png'")
         except Exception as e:
             print(f'Failed creating image: {e}')
 
