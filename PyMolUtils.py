03459a4440c4bfc989c9a686e2daec134593d3c8
bar246802 <bar246802@gmail.com>
2021-06-24 16:56:39 +0300

    Pymol Utils

--- PyMolUtils.py
+++ PyMolUtils.py
@@ -1,14 +1,13 @@
 import os
+import time
+from time import sleep
+
 import pymolPy3
 import platform
 import subprocess
 
-import os
-import pymolPy3
-import platform
-import subprocess
 
-def create_pdb_img(ref_pdb_path, target_pdb_path, out_path_dir):
+def create_pdb_img(ref_pdb_path, target_pdb_path_list, out_path_dir):
     if platform.system() == 'Linux':
         try:
             # Run command install
@@ -31,44 +30,35 @@ def create_pdb_img(ref_pdb_path, target_pdb_path, out_path_dir):
                 print(f"Error - Reference PDB file isn't found, please verify path: {ref_pdb_path}")
                 return
 
-            if not (target_pdb_path.endswith('.pdb')):
-                print(f"Error - Target PDB isn't of type .pdb")
-                return
-            if not (os.path.isfile(target_pdb_path)):
-                print(f"Error - Target PDB file isn't found, please verify path: {target_pdb_path}")
-                return
-
-            pm = pymolPy3.pymolPy3()
-            target_file_name = os.path.basename(target_pdb_path)
-            target_file_no_type = target_file_name.split(".")[0]
             ref_file_name = os.path.basename(ref_pdb_path)
             ref_file_no_type = ref_file_name.split(".")[0]
-            # Run commands
+            pm = pymolPy3.pymolPy3()
             pm(f"load {ref_pdb_path}")
             pm(f"color yellow, {ref_file_no_type}")
-            pm(f"load {target_pdb_path}")
-            pm(f"color green, {target_file_no_type}")
-            pm(f"align {ref_file_no_type}, {target_file_no_type}")
+
+            success_target_count = 0
+            for target_pdb_path in target_pdb_path_list:
+                if not ((os.path.isfile(target_pdb_path)) and target_pdb_path.endswith('.pdb')):
+                    continue
+                target_file_name = os.path.basename(target_pdb_path)
+                target_file_no_type = target_file_name.split(".")[0]
+                # Run commands
+                pm(f"load {target_pdb_path}")
+                pm(f"color green, {target_file_no_type}")
+                pm(f"align {ref_file_no_type}, {target_file_no_type}")
+                success_target_count += 1
+
+            if not success_target_count:
+                print(f"Error - could not find 1 Target PDB...")
+                return
+
             pm(f"center")
             pm(f"zoom")
             pm(f"png {out_path_dir}/align_img")
+            time.sleep(12)
             print(f"Done! the image is here: '{out_path_dir}/align_img.png'")
-
         except Exception as e:
             print(f'Failed creating image: {e}')
 
     else:
         print("Only work with Linux, sorry :/")
-
-def test():
-    output_directory = "./outputs"
-    file1 = output_directory + "/original.pdb"
-    file2 = output_directory + "/mutated_1.pdb"
-    create_pdb_img(file1, file2, output_directory)
-
-if __name__ == "__main__":
-    output_directory = "./outputs"
-    file1 = output_directory + "/original.pdb"
-    file2 = output_directory + "/mutated_1.pdb"
-    create_pdb_img(file1, file2, output_directory)
-    create_pdb_img(file1, file2, output_directory)
